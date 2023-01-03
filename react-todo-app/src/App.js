f3dec8df6248aaa1da19a8299ff2db461796fefc
oduodg <werewildfire@gmail.com>
2023-01-03 16:00:48 +0900

    :art: Separate a form component.
    Form 부분을 위한 컴포넌트 생성하기

--- react-todo-app/src/App.js
+++ react-todo-app/src/App.js
@@ -1,16 +1,13 @@
 import React, { useState } from "react";
 import "./App.css";
+import Form from "./components/Form";
 import List from "./components/List";
 
 export default function App() {
 
   const [todoData, setTodoData] = useState([]);
   const [value, setValue] = useState("");
-
-  const handleChange = (e) => {
-    setValue(e.target.value);
-  }
-
+  
   const handleSubmit = (e) => {
     // form 안에 input을 전송할 때 페이지 리로드 되는 것을 막아준다.
     e.preventDefault();
@@ -33,25 +30,8 @@ export default function App() {
         <div className="title">
           <h1>할 일 목록</h1>
         </div>
-
         <List todoData={todoData} setTodoData={setTodoData} />
-
-        <form style={{ display: 'flex' }} onSubmit={handleSubmit}>
-          <input
-            type="text"
-            name="value"
-            style={{ flex: "10", padding: "5px" }}
-            placeholder="해야 할 일을 입력하세요."
-            value={value}
-            onChange={handleChange}
-          />
-          <input
-            type="submit"
-            value="입력"
-            className="btn"
-            style={{ flex: "1" }}
-          />
-        </form>
+        <Form handleSubmit={handleSubmit} value={value} setValue={setValue} />
 
       </div>
     </div>
